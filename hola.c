bf30bfea770d664bfc0f1ff3513be06f7e45816d
ASUS USER <rt683591@iteso.mx>
2014-01-27 09:24:08 -0600

    fdfs

--- hola.c
+++ hola.c
@@ -7,15 +7,3 @@ void main(void) {
 void funcion2(void) {
 	printf("Funcion 2\n")
 }
-
-	#include <stdio.h>
-
-	void main(void) {
-		printf(“Hola Mundo\n”);
-	}
-
-
-
-void funcion1(void) {
-	printf("Funcion 1\n");
-}
