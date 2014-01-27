1a6edf558480f4bc901ab6ee1fde7ff4c846d685
ASUS USER <rt683591@iteso.mx>
2014-01-27 09:26:43 -0600

    sfsdfsd

--- hola.c
+++ hola.c
@@ -8,14 +8,10 @@ void funcion2(void) {
 	printf("Funcion 2\n")
 }
 
-	#include <stdio.h>
-
-	void main(void) {
-		printf(“Hola Mundo\n”);
-	}
-
-
-
 void funcion1(void) {
 	printf("Funcion 1\n");
+}
+
+void funcion3(void) {
+	printf("Funcion 3\n");
 }
